887781e6fe34c1af23352708ed24be9f53fe06dd
C_Adams <caspianadams37@gmail.com>
2025-06-02 10:23:09 +1000

    Added pages as well as the start of the code for tasks

---
+++ app.js
@@ -0,0 +1,32 @@
+const newtask = document.getElementsByTagName("form")[0];
+const TitleInput = document.getElementById("title");
+const DescInput = document.getElementById("desc");
+
+newtask.addEventListener("submit",(event) => {
+
+    event.preventDefault()
+
+    const TaskTitle = TitleInput.value;
+    const TaskDesc = DescInput.value;
+
+});
+
+function storeNewTask(TaskTitle, TaskDesc) {
+    const tasks = GetAllStoredTasks();
+
+    tasks.push({TaskTitle,TaskDesc});
+
+    window.alert("YARGH")
+    
+};
+
+function GetAllStoredTasks() {
+  // Get the string of session data from localStorage
+  const data = window.localStorage.getItem(STORAGE_KEY);
+
+  // If no sessions were stored, default to an empty array
+  // otherwise, return the stored data as parsed JSON
+  const sessions = data ? JSON.parse(data) : [];
+
+  return sessions;
+}
