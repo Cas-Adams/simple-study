ad57cef150fdf9d95b2880236a6b962ccf00eb52
C_Adams <caspianadams37@gmail.com>
2025-07-17 21:51:05 +1000

    changed back ticks on strings

--- main.js
+++ main.js
@@ -9,10 +9,10 @@ const updateCalendar = () => {
     const currentYear = currentDate.getFullYear();
     const currentMonth = currentDate.getMonth();
 
-    const firstDay = new Date(currentYear, currentMonth, 0)
-    const lastDay = new Date(currentYear, currentmonth + 1, 0)
+    const firstDay = new Date(currentYear, currentMonth, 0);
+    const lastDay = new Date(currentYear, currentMonth + 1, 0);
     const totalDays = lastDay.getDate();
-    const firstdayIndex = firstDay.getDay();
+    const firstDayIndex = firstDay.getDay();
     const lastDayIndex = lastDay.getDay();
 
     const monthYearString = currentDate.toLocaleString('default', {month: 'long', year: 'numeric'});
@@ -20,23 +20,23 @@ const updateCalendar = () => {
 
     let datesHTML = '';
 
-    for(let i = firstdayIndex; i > 0; i--) {
+    for(let i = firstDayIndex; i > 0; i--) {
         const prevDate = new Date(currentYear, currentMonth, 0 - i + 1);
-        datesHTML += '<div class="date inactive">${prevDate.getDate()}</div>';
-    }
+        datesHTML += `<div class="date inactive">${prevDate.getDate()}</div>`;
+    };
 
     for(let i = 1; i <= totalDays; i++) {
-        const date = new date(currentYear, currentMonth, i);
+        const date = new Date(currentYear, currentMonth, i);
         const activeClass = date.toDateString() === new Date().toDateString() ? 'active ' : ''; 
-        datesHTML += '<div class="date ${activeClass}">${i}</div>';
+        datesHTML += `<div class="date ${activeClass}">${i}</div>`;
         
-    }
+    };
 
-    for(let i=1; i <= 7 - lastDayIndex; i++) {
+    for(let i = 1; i <= 7 - lastDayIndex; i++) {
         const nextDate = new Date(currentYear, currentMonth + 1, i);
-        datesHTML += '<div class="date inactive">${nextdate.getDate()}</div>';
+        datesHTML += `<div class="date inactive">${nextDate.getDate()}</div>`;
 
-    }
+    };
 
     datesElement.innerHTML = datesHTML;
 }
